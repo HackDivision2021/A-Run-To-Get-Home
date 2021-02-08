008dc597b5b515df58e8710dc839ef3ca146cf64
Max Bay <cwang167@my.centennialcollege.ca>
2021-02-08 17:04:52 +0800

    Update from Max
    + Player control: move left, move right, roll
    + Functional pause button added

---
+++ Assets/Scripts/AnimationManger.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManger : MonoBehaviour
+{
+    public delegate void AnimationHandler();
+    public AnimationHandler animationHandler;
+
+    private Animation animation;
+    public AnimationClip
+        dead,
+        jumpDown,
+        jumpLoop,
+        jumpUp,
+        roll,
+        run,
+        turnLeft,
+        turnRight;
+
+    public static AnimationManger instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        animation = GetComponent<Animation>();
+        animationHandler = PlayRun;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //animation.Play(run.name);
+        if (animationHandler != null)
+        {
+            animationHandler.Invoke();
+        }
+
+    }
+
+    public void PlayDead() => animation.Play(dead.name);
+    public void PlayJumpDown() => animation.Play(jumpDown.name);
+    public void PlayJumpLoop() => animation.Play(jumpLoop.name);
+    public void PlayJumpUp() => animation.Play(jumpUp.name);
+
+    public void PlayTurnRight()
+    {
+        animation.Play(turnRight.name);
+        if(animation[turnRight.name].normalizedTime >= 0.95f)
+        {
+            animationHandler = PlayRun;
+        }
+    }
+    public void PlayTurnLeft() {
+        animation.Play(turnLeft.name);
+        if (animation[turnLeft.name].normalizedTime >= 0.95f)
+        {
+            animationHandler = PlayRun;
+        }
+    }
+    public void PlayRoll() {
+        animation.Play(roll.name);
+        if (animation[roll.name].normalizedTime >= 0.95f)
+        {
+            animationHandler = PlayRun;
+        }
+    }
+
+    public void PlayRun() => animation.Play(run.name);
+}
---
+++ Assets/Scripts/FloorSetting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorSetting : MonoBehaviour
+{
+    public GameObject currentFloor;
+    public GameObject nextFloor;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > currentFloor.transform.position.z+37)
+        {
+            currentFloor.transform.position = new Vector3(0, 0, nextFloor.transform.position.z + 32);
+            GameObject temp = currentFloor;
+            currentFloor = nextFloor;
+            nextFloor = temp;
+        }
+
+    }
+}
